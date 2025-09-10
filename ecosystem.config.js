c2f5bdbbb168971a9effaace139c77fb35d4079f
tonkhao <33198781+tonkhao@users.noreply.github.com>
2025-09-10 17:37:48 +0300

    ecosys add

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DEPLOY_REPOSITORY,
+} = process.env;
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'pwd && npm ci && npm run build',
+    },
+  },
+};
